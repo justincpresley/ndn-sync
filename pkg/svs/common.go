709e51b3a0dc886222c3ac41c65edcd24f057e74
Justin C Presley <justincpresley@gmail.com>
2022-11-19 15:24:01 -0600

    consolidate small files

--- pkg/svs/common.go
+++ pkg/svs/common.go
@@ -21,6 +21,16 @@
 
 package svs
 
+import (
+	enc "github.com/zjkmxy/go-ndn/pkg/encoding"
+)
+
+const (
+	TypeVector     enc.TLNum = 0xc9
+	TypeEntry      enc.TLNum = 0xca
+	TypeEntrySeqno enc.TLNum = 0xcc
+)
+
 type NamingScheme int
 
 const (
