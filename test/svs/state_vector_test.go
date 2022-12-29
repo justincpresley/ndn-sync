183cd34337af3b79098d69352178b25df8ee7723
Justin C Presley <justincpresley@gmail.com>
2022-12-29 01:53:05 -0600

    License Change

--- test/svs/state_vector_test.go
+++ test/svs/state_vector_test.go
@@ -1,24 +1,3 @@
-/*
- Copyright (C) 2022-2030, The ndn-sync Go Library Authors
-
- This file is part of ndn-sync: An NDN Go Library for Sync Protocols.
-
- ndn-sync is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or any later version.
-
- ndn-sync is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- See the GNU Lesser General Public License for more details.
-
- A copy of the GNU Lesser General Public License is provided by this
- library under LICENSE.md. If absent, it can be found within the
- GitHub repository:
-          https://github.com/justincpresley/ndn-sync
-*/
-
 package svs_test
 
 import (
